cc3ab71ff96904c117780adbef556facbb7aba8d
mjdave <davidframpton@majicjungle.com>
2019-09-09 23:00:14 +1200

    add description and preview

--- Examples/BigLeavesLuaWorldMod/modInfo.lua
+++ Examples/BigLeavesLuaWorldMod/modInfo.lua
@@ -1,5 +1,7 @@
 local modInfo = {
     name = "Big Leaves World Lua Mod",
+    description = "A very simple example mod that makes all the leaves bigger.",
+    preview = "preview.jpg",
     version = "0.0.1",
     type = "world",
 }
